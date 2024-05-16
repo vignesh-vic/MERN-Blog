d1267d25e9396484d7a0827ae78b760c2141395c
vignesh-vic <vignesha010@gmail.com>
2024-05-16 22:31:39 +0530

    updated

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
           <h1 className='text-3xl  font-semibold text-center my-7'>About Blog</h1>
           <div className='text-md text-gray-500 flex  flex-col gap-6'>
             <p>
-              The term "MERN stack" refers to a collection of technologies used together to build web applications. MERN stands for MongoDB, Express, React, and Node.js. Here's an overview of each component in the stack:
+              The Term "MERN stack" refers to a collection of technologies used together to build web applications. MERN stands for MongoDB, Express, React, and Node.js. Here's an overview of each component in the stack:
             </p>
           </div>
         </div>
