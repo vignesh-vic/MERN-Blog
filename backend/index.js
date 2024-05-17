d33453358f3cf6fef710713f27a4d89cc7f52491
vignesh-vic <vignesha010@gmail.com>
2024-05-17 09:14:28 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use('/api/comment', commentRoute)
 // const __dirname = path.resolve(); // Ensure this line is not duplicated
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend','build', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
