cb824c6ac31e67cf6b9c7d2007c2e9b9466d2052
vignesh-vic <vignesha010@gmail.com>
2024-05-17 08:16:03 +0530

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser')
-
+const path = require('path')
 const dotenv = require('dotenv')
 const app = express()
 app.use(cookieParser())
 
+
 const cors = require('cors');
+dotenv.config();
+
 app.use(cors());
 app.use(express.json())
-dotenv.config()
+
 const userRoute = require('./routes/userRoute')
 const authRoute = require('./routes/authRoute')
 const postRoute = require('./routes/postRoute')
@@ -22,6 +25,13 @@ app.use('/api/auth', authRoute)
 app.use('/api/post', postRoute)
 app.use('/api/comment', commentRoute)
 
+// Serve static files
+// const __dirname = path.resolve(); // Ensure this line is not duplicated
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal Server Error'
@@ -33,10 +43,10 @@ app.use((err, req, res, next) => {
 })
 
 
+
 app.listen(5000, () => {
     console.log("server running 5000 port");
 })
-
 
 
 connectDB();
